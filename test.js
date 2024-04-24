cae2d2f655b173cdba31869975d959c259a331ba
RodrigoAndresArriaga <67437170+RodrigoAndresArriaga@users.noreply.github.com>
2024-04-23 21:19:30 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000; // Default to 3000 if environment port not available
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
